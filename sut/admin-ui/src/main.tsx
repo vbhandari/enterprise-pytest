bb328ba60c27714742fa167dc8dd3cce3e49995f
Vinay Bhandari <vinayb@rocketmail.com>
2026-03-19 02:07:41 +0000

    change path in main.tsx

--- sut/admin-ui/src/main.tsx
+++ sut/admin-ui/src/main.tsx
@@ -6,7 +6,7 @@ import App from './App'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename="/admin">
+    <BrowserRouter basename="/static/admin">
       <App />
     </BrowserRouter>
   </StrictMode>,
